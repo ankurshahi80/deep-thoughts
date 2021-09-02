52ca5bc6da12ec8c5c15db2a640d8b113898519a
Ankur <ankurshahi@Ankurs-iMac.local>
2021-09-02 15:52:10 -0400

    me and add thoughts now working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,17 @@ const {signToken} = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async(parent, args, context)=>{
+          if(context.user) {
+            const userData = await User.findOne({_id:context.user._id})
+            .select('-__v -password')  
+            .populate('thoughts')
+            .populate('friends');
+
+            return userData;
+          }
+          throw new AuthenticationError('Not logged in');
+        },
         // get all users
         users: async()=>{
             return User.find()
@@ -48,6 +59,21 @@ const resolvers = {
 
             const token = signToken(user);
             return {token,user};
+        },
+        addThought: async(parent, args, context)=> {
+            if(context.user){
+                const thought = await Thougth.create({...args, username: context.user.username});
+
+                await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$push:{thoughts: thought._id}},
+                    {new: true}
+                );
+
+                return thought;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
